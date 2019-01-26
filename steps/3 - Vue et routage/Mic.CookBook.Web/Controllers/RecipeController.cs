f81014fad7684d9089c61e0b3229e0c77f2d2e03
Adrien Clerbois <adrien.clerbois@outlook.com>
2019-01-26 21:00:41 +0100

    Add Step 3

---
+++ steps/3 - Vue et routage/Mic.CookBook.Web/Controllers/RecipeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using Mic.CookBook.Web.Database;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mic.CookBook.Web.Controllers
+{
+    public class RecipeController : Controller
+    {
+        private readonly CookBookContext cookBookContext;
+
+        public RecipeController(CookBookContext cookBookContext)
+        {
+            this.cookBookContext = cookBookContext;
+        }
+
+        public IActionResult Index()
+        {
+            var list = cookBookContext.Recipes.ToList();
+            return View(list);
+        }
+
+        public IActionResult Details(int id, string name = null)
+        {
+            var recipe = cookBookContext.Recipes.FirstOrDefault(r => r.Id == id);
+            return View(recipe);
+        }
+    }
+}
