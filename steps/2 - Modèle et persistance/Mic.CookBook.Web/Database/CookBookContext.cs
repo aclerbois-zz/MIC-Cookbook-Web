2b7bd8262341c6cc61cc1671196adfab36d01484
Adrien Clerbois <adrien.clerbois@outlook.com>
2019-01-26 19:36:36 +0100

    Add Step 2

---
+++ steps/2 - Modèle et persistance/Mic.CookBook.Web/Database/CookBookContext.cs
@@ -0,0 +1,12 @@
+﻿using Mic.CookBook.Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mic.CookBook.Web.Database
+{
+    public class CookBookContext : DbContext
+    {
+        public CookBookContext(DbContextOptions<CookBookContext> options) : base(options) { }
+
+        public DbSet<Recipe> Recipes { get; set; }
+    }
+}
