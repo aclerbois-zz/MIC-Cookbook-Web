3804e1a92da8ff6717e91b5b43ecd3145a2b0cfb
Adrien Clerbois <adrien.clerbois@outlook.com>
2019-01-26 23:21:13 +0100

    Step 5

---
+++ steps/5 - Area & Administration/Mic.CookBook.Web/Startup.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Mic.CookBook.Web.Database;
+using System.Data.Common;
+using Mic.CookBook.Web.Controllers;
+
+namespace Mic.CookBook.Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddSwaggerDocument();
+
+            var connectionString = @"Server=tcp:mic-cookbook.database.windows.net,1433;Initial Catalog=demo;Persist Security Info=False;User ID=aclerbois;Password=*******;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            services.AddDbContext<CookBookContext>(options => options.UseSqlServer(connectionString));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseSwagger();
+            app.UseSwaggerUi3();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "recettes",
+                    template: "recettes",
+                    defaults: new { controller = "Recipe", action = "Index" });
+                routes.MapRoute(
+                    name: "recette-details",
+                    template: "recette/{id}/{name}",
+                    defaults: new { controller = "Recipe", action = "Details" });
+                routes.MapRoute(
+                    name: "areas",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
