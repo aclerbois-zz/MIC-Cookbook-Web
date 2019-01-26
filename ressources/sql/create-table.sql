886d77435975b2c1f1bdfb6eafa3016deeb8cf93
Adrien Clerbois <adrien.clerbois@outlook.com>
2019-01-26 15:09:58 +0100

    Add SQL scripts

---
+++ ressources/sql/create-table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Recipe(
+    [Name]          VARCHAR(30),
+    [Location]      VARCHAR(30),
+    [Details]       VARCHAR(30),
+    [Image]         VARCHAR(255),
+    [Category]      VARCHAR(30),
+    [Population]    INTEGER,
+    [Latitude]      NUMERIC(30,6),
+    [Longitude]     NUMERIC(30,6)
+);
